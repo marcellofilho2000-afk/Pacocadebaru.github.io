bb4d20e67863ede41736df20889279437ef117ae
marcellofilho2000-afk <marcellofilho2000@gmail.com>
2026-06-19 11:28:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
+const btn = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
 
-    const menuToggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".menu");
-
-    if (menuToggle && menu) {
-
-        menuToggle.addEventListener("click", function () {
-
-            menu.classList.toggle("active");
-
-            const expanded =
-                menuToggle.getAttribute("aria-expanded") === "true";
-
-            menuToggle.setAttribute(
-                "aria-expanded",
-                !expanded
-            );
-
-        });
-
-    }
-
+btn.addEventListener("click", () => {
+    btn.classList.toggle("active");
+    menu.classList.toggle("active");
 });
