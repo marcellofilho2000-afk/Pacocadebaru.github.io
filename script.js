b59953f1c91ee458747dd6b91aa01ff79bdfcb64
marcellofilho2000-afk <marcellofilho2000@gmail.com>
2026-06-19 11:46:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
+const menuToggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
 
-    const menuBtn = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".menu");
-
-    menuBtn.addEventListener("click", () => {
-        menuBtn.classList.toggle("active");
-        menu.classList.toggle("active");
-    });
-
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    menuToggle.classList.toggle("active");
 });
